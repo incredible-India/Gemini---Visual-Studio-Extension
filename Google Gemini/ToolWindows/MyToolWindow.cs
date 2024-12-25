6f22f5da3e88e44e76b114c512372a1d79d8f8d3
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-12-25 11:36:46 +0530

    replaced and added new

---
+++ Google Gemini/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Google_Gemini
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "Google Gemini";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("17e5c7e3-6b81-47cc-b442-16e8d3060a03")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+                Caption = "Google Gemini";
+            }
+
+        }
+    
+    }
+}
